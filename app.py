ea13cad7bc975c48424e3a8514d5bfea58991b01
Sunil Kumar <sunil.kumar@nescode.com>
2020-05-30 09:11:36 +0000

    displaying row data

--- app.py
+++ app.py
@@ -1,4 +1,27 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.title("Hello world!")
-st.markdown("## My first streamlit dashboard!")
+# st.title("Hello world!")
+# st.markdown("## My first streamlit dashboard!")
+
+DATA_URL = (
+ "/home/rhyme/Desktop/motorvechile-mlds/mvcrashes.csv"
+)
+st.title("Motor vehicle collision in new york city")
+st.markdown("This application is a streamlit application to analyse motor vehicle collision in nyc")
+
+@st.cache(persist=True)
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows, parse_dates=[['CRASH_DATE', 'CRASH_TIME']])
+    data.dropna(subset=['LATITUDE', 'LONGITUDE'], inplace=True)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data.rename(columns={'crash_date_crash_time': 'date/time'}, inplace=True)
+    return data
+
+data = load_data(100000)
+
+if st.checkbox("Show raw data", False):
+    st.subheader('Row data')
+    st.write(data)
