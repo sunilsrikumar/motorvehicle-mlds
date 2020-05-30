e6ef463ee0c5af29f00476f91651e5c72333f21b
Sunil Kumar <sunil.kumar@nescode.com>
2020-05-30 17:32:23 +0530

    select data using dropdown

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ def load_data(nrows):
     return data
 
 data = load_data(100000)
+original_data = data
 
 st.header("Where are the most people injured in NYC?")
 injured_people = st.slider("Number of persons injured in vechile collisions", 0, 19)
@@ -66,6 +67,19 @@ hist = np.histogram(filtered['date/time'].dt.minute, bins=60, range=(0, 60))[0]
 chart_data = pd.DataFrame({'minute': range(60), 'crashes':hist})
 fig = px.bar(chart_data, x='minute', y='crashes', hover_data=['minute', 'crashes'], height=400)
 st.write(fig)
+
+st.header("Top 5 dangerious streets by affected type")
+select = st.selectbox('Affected type of people', ['Pedestrians', 'Cyclists', 'Motorists'])
+
+if select == 'Pedestrians':
+    st.write(original_data.query("injured_pedestrians >= 1")[["on_street_name", "injured_pedestrians"]].sort_values(by=['injured_pedestrians'], ascending=False).dropna(how='any')[:5])
+
+elif select == 'Cyclists':
+    st.write(original_data.query("injured_cyclists >= 1")[["on_street_name", "injured_cyclists"]].sort_values(by=['injured_cyclists'], ascending=False).dropna(how='any')[:5])
+
+else:
+    st.write(original_data.query("injured_motorists >= 1")[["on_street_name", "injured_motorists"]].sort_values(by=['injured_motorists'], ascending=False).dropna(how='any')[:5])
+
 
 if st.checkbox("Show raw data", False):
     st.subheader('Row data')
